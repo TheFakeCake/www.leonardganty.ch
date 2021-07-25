969c584f2bf05b8873951a687841cdb77eb242e4
TheFakeCake <TheFakeCake@users.noreply.github.com>
2021-07-25 16:50:56 +0200

    Augmente le padding gauche-droite de <ContentSection>

--- components/ContentSection.jsx
+++ components/ContentSection.jsx
@@ -1,6 +1,6 @@
 export default function ContentSection({ title, children }) {
   return (
-    <section className="px-2 py-4 text-center">
+    <section className="px-3 py-4 text-center">
       <h1 className="inline-block text-3xl border-b-2 border-red-800 pb-1 mb-4">
         {title}
       </h1>
