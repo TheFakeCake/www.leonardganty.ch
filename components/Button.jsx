7043c9e74abbb0a0c871f56283047497fae40858
TheFakeCake <TheFakeCake@users.noreply.github.com>
2021-10-14 12:24:31 +0200

    Corrige et améliore le comportement du Button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const classes = `
-  inline-block px-6 py-2 rounded-md bg-primary text-gray-100 font-sans ring-primary-dark transition
-  hover:bg-primary-light hover:shadow-lg
+  inline-block px-6 py-2 rounded-md bg-primary text-gray-100 font-sans ring-primary-dark transition select-none
+  hover:bg-primary-light hover:focus-not-visible:bg-primary-light hover:shadow-lg
   focus:bg-primary-light focus:shadow-lg focus:outline-none focus:ring-2
-  focus-not-visible:ring-0
-  active:bg-primary-dark active:shadow-sm
+  focus-not-visible:bg-primary focus-not-visible:ring-0 focus-not-visible:shadow-none
+  active:hover:bg-primary-dark active:hover:shadow-md active:hover:focus-not-visible:bg-primary-dark active:hover:focus-not-visible:shadow-md
 `
   .trim()
   .split(/\s+/)
@@ -14,7 +14,14 @@ const classes = `
 const Button = React.forwardRef(({ href, onClick, type, children }, ref) => {
   if (href) {
     return (
-      <a className={classes} href={href} onClick={onClick} ref={ref}>
+      <a
+        className={classes}
+        href={href}
+        onClick={onClick}
+        draggable="false"
+        onDragStart={(e) => e.preventDefault()} // Fix for Firefox
+        ref={ref}
+      >
         {children}
       </a>
     );
