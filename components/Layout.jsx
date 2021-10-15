39bbd04f95eb1e6ac6317f0c3fd404a85bfc6fe8
TheFakeCake <TheFakeCake@users.noreply.github.com>
2021-10-15 23:00:31 +0200

    Ajoute un style à la sélection de texte

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import PageHeader from './PageHeader';
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen pt-16 font-serif bg-gray-100">
+    <div className="flex flex-col min-h-screen pt-16 font-serif bg-gray-100 selection:bg-primary-light selection:text-gray-50">
       <PageHeader />
       <main>{children}</main>
       <PageFooter />
