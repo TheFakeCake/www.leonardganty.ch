b4b384bf29174b72baf1c33466a53fa2d5a69844
TheFakeCake <TheFakeCake@users.noreply.github.com>
2021-11-03 12:02:19 +0100

    Refactor la page Mon parcours avec <ContentRow> et <ContentColumn>

--- pages/mon-parcours.jsx
+++ pages/mon-parcours.jsx
@@ -1,55 +1,58 @@
-import ContentSection from '../components/ContentSection';
+import ContentColumn from '../components/layout/ContentColumn';
+import ContentRow from '../components/layout/ContentRow';
+import Heading from '../components/Heading';
 import Quotation from '../components/Quotation';
 
 export default function MonParcours() {
   return (
-    <ContentSection
-      className="mb-footer-wave-height lg:mt-4"
-      heading="Mon parcours"
-    >
-      <p className="my-2">
-        Physiothérapeute de formation, j’ai eu la chance de pratiquer la
-        physiothérapie du sport en cabinet et avec des sportifs professionnels
-        tel que la relève du <em>Genève-Servette Hockey Club</em>.
-      </p>
+    <ContentRow tag="section">
+      <ContentColumn>
+        <Heading>Mon parcours</Heading>
 
-      <p className="my-2">
-        Après ces expériences, j’ai rapidement entamé mon cursus de formation
-        en médecine traditionnelle chinoise pour lequel j’ai obtenu mon
-        diplôme auprès du{' '}
-        <a className="link link-primary" href="https://esc-suisse.ch/">
-          Europe&#8209;Shanghai College of TCM
-        </a>
-        .
-      </p>
+        <p className="my-2">
+          Physiothérapeute de formation, j’ai eu la chance de pratiquer la
+          physiothérapie du sport en cabinet et avec des sportifs professionnels
+          tel que la relève du <em>Genève-Servette Hockey Club</em>.
+        </p>
 
-      <p className="my-2">
-        J’ai choisi de me tourner vers cette médecine afin de pouvoir ouvrir
-        le champ de mes possibilités de prises en charge. Je peux ainsi
-        accompagner des personnes qui souffrent de problématiques pour
-        lesquelles je n’ai pas d’outils en tant que physiothérapeute, comme
-        par exemple les insomnies, le stress ou les troubles menstruels.
-      </p>
+        <p className="my-2">
+          Après ces expériences, j’ai rapidement entamé mon cursus de formation
+          en médecine traditionnelle chinoise pour lequel j’ai obtenu mon
+          diplôme auprès du{' '}
+          <a className="link link-primary" href="https://esc-suisse.ch/">
+            Europe&#8209;Shanghai College of TCM
+          </a>
+          .
+        </p>
 
-      <p className="my-2">
-        Aujourd’hui, j’ai la chance de pouvoir vous accueillir en cabinet pour
-        des traitements d’acupuncture ou de venir directement chez vous si
-        besoin.
-      </p>
+        <p className="my-2">
+          J’ai choisi de me tourner vers cette médecine afin de pouvoir ouvrir
+          le champ de mes possibilités de prises en charge. Je peux ainsi
+          accompagner des personnes qui souffrent de problématiques pour
+          lesquelles je n’ai pas d’outils en tant que physiothérapeute, comme
+          par exemple les insomnies, le stress ou les troubles menstruels.
+        </p>
 
-      <p className="my-2">
-        En plus de l’acupuncture, je poursuis également mon activité de
-        physiothérapeute aux <em>Hôpitaux Universitaires de Genève</em> où je
-        prends en charge des patients ayant subi des accidents ou
-        interventions chirurgicales orthopédiques.
-      </p>
+        <p className="my-2">
+          Aujourd’hui, j’ai la chance de pouvoir vous accueillir en cabinet pour
+          des traitements d’acupuncture ou de venir directement chez vous si
+          besoin.
+        </p>
 
-      <Quotation
-        className="mt-6 mr-3 text-right sm:mr-0"
-        text="Plus le sage donne aux autres, plus il possède."
-        author="Lao Tseu"
-        source="Dao de jing"
-      />
-    </ContentSection>
+        <p className="my-2">
+          En plus de l’acupuncture, je poursuis également mon activité de
+          physiothérapeute aux <em>Hôpitaux Universitaires de Genève</em> où je
+          prends en charge des patients ayant subi des accidents ou
+          interventions chirurgicales orthopédiques.
+        </p>
+
+        <Quotation
+          className="mt-6 mr-3 text-right sm:mr-0"
+          text="Plus le sage donne aux autres, plus il possède."
+          author="Lao Tseu"
+          source="Dao de jing"
+        />
+      </ContentColumn>
+    </ContentRow>
   );
 }
