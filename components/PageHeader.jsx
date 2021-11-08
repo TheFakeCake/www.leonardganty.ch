0edfcdbef2ecdb1cf755639ecea52cb1cfb38193
TheFakeCake <TheFakeCake@users.noreply.github.com>
2021-11-08 21:27:15 +0100

    Implémente le focus trap de la navigation sur mobile

--- components/PageHeader.jsx
+++ components/PageHeader.jsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import FocusTrap from 'focus-trap-react';
+
+import { debounce } from '../lib/utils';
 
 import BurgerButton from './BurgerButton';
 import Logo from './Logo';
@@ -52,6 +55,20 @@ function PageHeader() {
     };
   });
 
+  // Add a debounced window resize handler to hide the navigation if the window
+  // becomes 1024px wide
+  useEffect(() => {
+    const onResizeHandler = debounce(({}) => {
+      if (window.innerWidth >= 1024) {
+        hideNav();
+      }
+    }, 300, 1000);
+
+    window.addEventListener('resize', onResizeHandler);
+
+    return () => window.removeEventListener('resize', onResizeHandler);
+  });
+
   const navVisibleClass =
     navVisible === null
       ? 'invisible'
@@ -64,61 +81,63 @@ function PageHeader() {
   }
 
   return (
-    <header
-      className="fixed top-0 flex justify-between w-full h-16 px-4 py-1 text-gray-100 shadow-md z-max bg-primary lg:justify-center"
-      role="banner"
-    >
-      <Link href="/">
-        <a className="link self-center rounded-full text-yellow-200 focus:ring-offset-0 hover:opacity-100 lg:hidden">
-          <Logo className="w-12 h-12 text-primary-dark border-black bg-gray-50" />
-        </a>
-      </Link>
-
-      <BurgerButton
-        className="self-center lg:hidden"
-        toggled={navVisible}
-        onClick={toggleNavVisible}
-        aria-controls="mainNav"
-        aria-label="Permuter l'affichage de la navigation"
-      />
-
-      <nav
-        className={
-          'fixed top-16 bottom-0 left-0 right-0 pt-10 pr-4 bg-primary ' +
-          'lg:static lg:pr-0 lg:pt-0 lg:w-full lg:max-w-2xl lg:visible lg:opacity-100 lg:animate-none ' +
-          navVisibleClass
-        }
-        id="mainNav"
+    <FocusTrap active={navVisible}>
+      <header
+        className="fixed top-0 flex justify-between w-full h-16 px-4 py-1 text-gray-100 shadow-md z-max bg-primary lg:justify-center"
+        role="banner"
       >
-        <ul className="flex flex-col text-3xl leading-relaxed lg:h-full lg:flex-row lg:justify-between lg:items-center lg:text-lg">
-          {navItems.map(({ href, label }) => {
-            return (
-              <li className="mb-6 text-right lg:mb-0 lg:-mt-1.5" key={href}>
-                <Link href={href}>
-                  <a
-                    className={
-                      'pb-2 border-b-2 ring-yellow-200 ring-offset-primary transition ' +
-                      'hover:text-yellow-200 hover:border-yellow-200 ' +
-                      'focus:outline-none focus:ring-2 focus:ring-offset-4 ' +
-                      'focus-not-visible:ring-0 focus-not-visible:ring-offset-0 ' +
-                      'lg:pb-0.5 lg:border-b-0' +
-                      (router.pathname === href
-                        ? ' font-semibold text-yellow-200 border-yellow-200 lg:font-normal lg:border-b-2'
-                        : '')
-                    }
-                    aria-current={router.pathname === href ? 'page' : false}
-                  >
-                    {label}
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+        <Link href="/">
+          <a className="link self-center rounded-full text-yellow-200 focus:ring-offset-0 hover:opacity-100 lg:hidden">
+            <Logo className="w-12 h-12 text-primary-dark border-black bg-gray-50" />
+          </a>
+        </Link>
 
-      <Logo className="hidden lg:block absolute top-13 -mt-0.5 w-10 h-10 text-primary-dark bg-gray-50 border-primary-dark shadow-md" />
-    </header>
+        <BurgerButton
+          className="self-center lg:hidden"
+          toggled={navVisible}
+          onClick={toggleNavVisible}
+          aria-controls="mainNav"
+          aria-label="Permuter l'affichage de la navigation"
+        />
+
+        <nav
+          className={
+            'fixed top-16 bottom-0 left-0 right-0 pt-10 pr-4 bg-primary ' +
+            'lg:static lg:pr-0 lg:pt-0 lg:w-full lg:max-w-2xl lg:visible lg:opacity-100 lg:animate-none ' +
+            navVisibleClass
+          }
+          id="mainNav"
+        >
+          <ul className="flex flex-col text-3xl leading-relaxed lg:h-full lg:flex-row lg:justify-between lg:items-center lg:text-lg">
+            {navItems.map(({ href, label }) => {
+              return (
+                <li className="mb-6 text-right lg:mb-0 lg:-mt-1.5" key={href}>
+                  <Link href={href}>
+                    <a
+                      className={
+                        'pb-2 border-b-2 ring-yellow-200 ring-offset-primary transition ' +
+                        'hover:text-yellow-200 hover:border-yellow-200 ' +
+                        'focus:outline-none focus:ring-2 focus:ring-offset-4 ' +
+                        'focus-not-visible:ring-0 focus-not-visible:ring-offset-0 ' +
+                        'lg:pb-0.5 lg:border-b-0' +
+                        (router.pathname === href
+                          ? ' font-semibold text-yellow-200 border-yellow-200 lg:font-normal lg:border-b-2'
+                          : '')
+                      }
+                      aria-current={router.pathname === href ? 'page' : false}
+                    >
+                      {label}
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        <Logo className="hidden lg:block absolute top-13 -mt-0.5 w-10 h-10 text-primary-dark bg-gray-50 border-primary-dark shadow-md" />
+      </header>
+    </FocusTrap>
   );
 }
 
